68a4fcd727458308cb717a6e45ac1cc4b291cef7
ahmed saeed <ahmed.saeed@sia-world.com>
2023-03-11 04:28:08 +0200

    init: laravel environment

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+}
